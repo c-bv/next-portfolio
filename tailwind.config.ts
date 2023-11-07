d0650ddc6610a726049ac34f284d38fc72b75605
c-bv <charles.bouveret.dev@gmail.com>
2023-11-07 16:32:31 +0100

    🎉 init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,11 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './content/**/*.{md,mdx}'],
+    darkMode: ['class'],
+    theme: {
+        extend: {}
     },
-  },
-  plugins: [],
-}
-export default config
+    plugins: [require('@tailwindcss/typography')]
+};
+export default config;
